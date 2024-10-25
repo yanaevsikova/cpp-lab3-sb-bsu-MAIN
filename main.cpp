b5e0ce36ca7b92f59a810a4c0bbb56d6c8fb46a2
yanaevsikova <yanaevsikova738@gmail.com>
2024-10-25 14:37:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    int k, i = 1, sum_k = 0, number;
-
-    // Prompt the user to enter the amount of integers to sum
-    cout << "Enter the amount of integers to sum: ";
-    cin >> k;
-
-    // Check if k is zero
-    if (k == 0) {
-        cout << "The total sum of 0 integers is: 0" << endl;
-        return 0; // Exit the program
-    }
-
-    // Loop to get k integers from the user
-    do {
-        cout << "Enter integer number " << i << ": ";
+int main()
+{
+  int k, i = 1, sum_k = 0, number;
+  cout << " Enter the amount of integers to sum = "; cin >> k;
+  if (k == 0) cout << "error";
+  else {
+    if (std::cin >> k) cout << "warning";
+    else {
+      do {
+        cout << "Enter integer nr. " << i << ": ";
         cin >> number;
-        sum_k += number; // Add the entered number to the sum
-        i++; // Increment the counter
-    } while (i <= k);
+        sum_k += number; i++;
+      } while (i <= k);
+      cout << " The total sum of " << k << " integers is: " << sum_k;
+    }
+  }
+  return 0;
 
-    // Output the total sum
-    cout << "The total sum of " << k << " integers is: " << sum_k << endl;
-
-    return 0;
 }
